5cccd43230892ae51b011a9d0c9989b95d06c0bd
shivenbajaj28 <67266804+shivenbajaj28@users.noreply.github.com>
2020-11-04 19:04:41 +0530

    Add files via upload

---
+++ CHESS GAME/JS/main.js
@@ -0,0 +1,115 @@
+//BoardSquares
+
+//row "A"
+var A1 = createSprite(displayWidth/2-200, displayHeight/2-200, 20, 20);
+var A2 = createSprite(displayWidth/2-180, displayHeight/2-200, 20, 20);
+var A3 = createSprite(displayWidth/2-160, displayHeight/2-200, 20, 20);
+var A4 = createSprite(displayWidth/2-140, displayHeight/2-200, 20, 20);
+var A5 = createSprite(displayWidth/2-120, displayHeight/2-200, 20, 20);
+var A6 = createSprite(displayWidth/2-100, displayHeight/2-200, 20, 20);
+var A7 = createSprite(displayWidth/2-80, displayHeight/2-200, 20, 20);
+var A8 = createSprite(displayWidth/2-60, displayHeight/2-200, 20, 20);
+//row "B"
+var B1 = createSprite(displayWidth/2-200, displayHeight/2-180, 20, 20);
+var B2 = createSprite(displayWidth/2-180, displayHeight/2-180, 20, 20);
+var B3 = createSprite(displayWidth/2-160, displayHeight/2-180, 20, 20);
+var B4 = createSprite(displayWidth/2-140, displayHeight/2-200, 20, 20);
+var B5 = createSprite(displayWidth/2-120, displayHeight/2-180, 20, 20);
+var B6 = createSprite(displayWidth/2-100, displayHeight/2-180, 20, 20);
+var B7 = createSprite(displayWidth/2-80, displayHeight/2-180, 20, 20);
+var B8 = createSprite(displayWidth/2-60, displayHeight/2-180, 20, 20);
+//row "C"
+var C1 = createSprite(displayWidth/2-200, displayHeight/2-160, 20, 20);
+var C2 = createSprite(displayWidth/2-180, displayHeight/2-160, 20, 20);
+var C3 = createSprite(displayWidth/2-160, displayHeight/2-160, 20, 20);
+var C4 = createSprite(displayWidth/2-140, displayHeight/2-160, 20, 20);
+var C5 = createSprite(displayWidth/2-120, displayHeight/2-160, 20, 20);
+var C6 = createSprite(displayWidth/2-100, displayHeight/2-160, 20, 20);
+var C7 = createSprite(displayWidth/2-80, displayHeight/2-160, 20, 20);
+var C8 = createSprite(displayWidth/2-60, displayHeight/2-160, 20, 20);
+//row "D"
+var D1 = createSprite(displayWidth/2-200, displayHeight/2-140, 20, 20);
+var D2 = createSprite(displayWidth/2-180, displayHeight/2-140, 20, 20);
+var D3 = createSprite(displayWidth/2-160, displayHeight/2-140, 20, 20);
+var D4 = createSprite(displayWidth/2-140, displayHeight/2-140, 20, 20);
+var D5 = createSprite(displayWidth/2-120, displayHeight/2-140, 20, 20);
+var D6 = createSprite(displayWidth/2-100, displayHeight/2-140, 20, 20);
+var D7 = createSprite(displayWidth/2-80, displayHeight/2-140, 20, 20);
+var D8 = createSprite(displayWidth/2-60, displayHeight/2-140, 20, 20);
+//row "E"
+var E1 = createSprite(displayWidth/2-200, displayHeight/2-120, 20, 20);
+var E2 = createSprite(displayWidth/2-180, displayHeight/2-120, 20, 20);
+var E3 = createSprite(displayWidth/2-160, displayHeight/2-120, 20, 20);
+var E4 = createSprite(displayWidth/2-140, displayHeight/2-120, 20, 20);
+var E5 = createSprite(displayWidth/2-120, displayHeight/2-120, 20, 20);
+var E6 = createSprite(displayWidth/2-100, displayHeight/2-120, 20, 20);
+var E7 = createSprite(displayWidth/2-80, displayHeight/2-120, 20, 20);
+var E8 = createSprite(displayWidth/2-60, displayHeight/2-120, 20, 20);
+//row "F"
+var F1 = createSprite(displayWidth/2-200, displayHeight/2-100, 20, 20);
+var F2 = createSprite(displayWidth/2-180, displayHeight/2-100, 20, 20);
+var F3 = createSprite(displayWidth/2-160, displayHeight/2-100, 20, 20);
+var F4 = createSprite(displayWidth/2-140, displayHeight/2-100, 20, 20);
+var F5 = createSprite(displayWidth/2-120, displayHeight/2-100, 20, 20);
+var F6 = createSprite(displayWidth/2-100, displayHeight/2-100, 20, 20);
+var F7 = createSprite(displayWidth/2-80, displayHeight/2-100, 20, 20);
+var F8 = createSprite(displayWidth/2-60, displayHeight/2-100, 20, 20);
+//row "G"
+var G1 = createSprite(displayWidth/2-200, displayHeight/2-80, 20, 20);
+var G2 = createSprite(displayWidth/2-180, displayHeight/2-80, 20, 20);
+var G3 = createSprite(displayWidth/2-160, displayHeight/2-80, 20, 20);
+var G4 = createSprite(displayWidth/2-140, displayHeight/2-80, 20, 20);
+var G5 = createSprite(displayWidth/2-120, displayHeight/2-80, 20, 20);
+var G6 = createSprite(displayWidth/2-100, displayHeight/2-80, 20, 20);
+var G7 = createSprite(displayWidth/2-80, displayHeight/2-80, 20, 20);
+var G8 = createSprite(displayWidth/2-60, displayHeight/2-80, 20, 20);
+//row "H"
+var H1 = createSprite(displayWidth/2-200, displayHeight/2-60, 20, 20);
+var H2 = createSprite(displayWidth/2-180, displayHeight/2-60, 20, 20);
+var H3 = createSprite(displayWidth/2-160, displayHeight/2-60, 20, 20);
+var H4 = createSprite(displayWidth/2-140, displayHeight/2-60, 20, 20);
+var H5 = createSprite(displayWidth/2-120, displayHeight/2-60, 20, 20);
+var H6 = createSprite(displayWidth/2-100, displayHeight/2-60, 20, 20);
+var H7 = createSprite(displayWidth/2-80, displayHeight/2-60, 20, 20);
+var H8 = createSprite(displayWidth/2-60, displayHeight/2-60, 20, 20);
+
+//green and white squares
+var GREENSQ = [A1, A3, A5, A7, B1, B3, B5, B7, C1, C3, C5, C7, D1, D3, D5, D7,
+                 E1, E3, E5, E7, F1, F3, F5, F7, G1, G3, G5, G7, H1, H3, H5, H7];
+var WHITESQ = [A2, A4, A6, A8, B2, B4, B6, B8, C2, C4, C6, C8, D2, D4, D6, D8,
+                 E2, E4, E6, E8, F2, F4, F6, F8, G2, G4, G6, G8, H2, H4, H6, H8];
+
+GREENSQ.shapeColor("green");
+WHITESQ.shapeColor("white");
+
+//pawn's arrays
+var WHITE_PAWN = [A2, B2, C2, D2, E2, F2, G2, H2];
+var BLACK_PAWN = [A7, B7, C7, D7, E7, F7, G7, H7];
+
+//knight's arrays
+var WHITE_KNIGHT = [B1, G1];
+var BLACK_KNIGHT = [B8, G8];
+
+//bishop's arrays
+var WHITE_BISHOP = [C1, F1];
+var BLACK_BISHOP = [C8, F8];
+
+//rook's arrays
+var WHITE_ROOK = [A1, H1];
+var BLACK_ROOK = [A8, H8];
+
+//queen's arrays
+var WHITE_QUEEN = [D1];
+var BLACK_QUEEN = [D8];
+
+//king's arrays
+var WHITE_KING = [E1];
+var BLACK_KING = [E8];
+
+//Boolean Value
+var BOOL = {FALSE:0, TRUE:1};
+
+if(mousePressedOver(GREENSQ || WHITESQ)){
+    GREENSQ.shapeColor("dark green");
+    WHITESQ.shapeColor("dark green");
+}
